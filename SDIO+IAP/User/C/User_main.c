e495ae163b2da1f315cf7759e3c753c6a5e1a383
techguy10k <47449920+techguy10k@users.noreply.github.com>
2019-08-31 03:36:27 +0800

    第一个能用的版本

--- SDIO+IAP/User/C/User_main.c
+++ SDIO+IAP/User/C/User_main.c
@@ -9,114 +9,159 @@
 //FatFs测试头文件
 #include "fatfs.h"
 #include "ff.h"
-uint8_t String[] = {"SD.txt"};
-uint8_t ReadBuffer[7] = {0};
-uint8_t ReadBufferS[10] = {0};
-uint8_t ReadBufferSS[10] = {0};
-uint8_t ReadBufferDir[20] = {0};
-uint32_t ReadOutCount = 0;
-
+#include "stdlib.h"
+uint8_t String[20] = {0};
+uint16_t SdVersion = 0;
 //FLASH测试头文件
 
 
 //printf串口需要的头文件
 #include "usart.h"
 
+//还没有封装的函数
+uint8_t IAP_FlashErase_APP(uint32_t ImageSize);
+uint8_t IAP_FlashProgram(FIL* file);
+void IAP_Jump2App(uint32_t AppFlashBase,uint32_t MSP_Addr);
+
 void User_main(void)
 {
 	
 	while(1)
 	{
-		/* 根目录单文件二进制读写操作测试 */
-		HAL_Delay(500);
-		printf("OpCodeMount %d \r\n",f_mount(&SDFatFS,(const TCHAR*)SDPath,1));
-		HAL_Delay(50);//东西太多HC12发不完 下面的50ms延迟同理
-		/* 打开文件 读取文件 获取当前文件指针 移动文件指针到0处 写入字符串 */
-		printf("OpCodeOpen  %d \r\n",f_open(&SDFile,(const TCHAR*)String,FA_READ | FA_WRITE));
-		HAL_Delay(50);
-		printf("OpCodeRead  %d \r\n",f_read(&SDFile,ReadBuffer,6,(unsigned int*)ReadOutCount));
-		HAL_Delay(50);
-		printf("ReadOut     %s \r\n",ReadBuffer);
-		HAL_Delay(50);
-		printf("OpCodeTell  %d \r\n",(int)f_tell(&SDFile));
-		HAL_Delay(50);
-		printf("OpCodelseek %d \r\n",f_lseek(&SDFile,0));
-		HAL_Delay(50);
-		printf("OpCodeTell  %d \r\n",(int)f_tell(&SDFile));
-		HAL_Delay(50);
-		printf("OpCodeWrite %d \r\n",f_write(&SDFile,String,7,(unsigned int*)ReadOutCount));
-		HAL_Delay(50);
-		/* 同步文件 */
-		printf("OpCodeSync  %d \r\n",f_sync(&SDFile));
-		/* 根目录单文件字符读写操作测试 如果不是二进制文件 建议使用此等操纵方式 */
-		printf("OpCodeReadS    \r\n");
-		HAL_Delay(50);
-		f_lseek(&SDFile,0);
-		ReadOutCount = 7;
-		f_gets((TCHAR*)ReadBufferS,ReadOutCount,&SDFile);
-		printf("ReadOut     %s \r\n",ReadBufferS);
-		HAL_Delay(50);
-		printf("OpCodeWriteS    \r\n");
-		HAL_Delay(50);
-		f_lseek(&SDFile,0);
-		f_printf(&SDFile,"SD.txt");
-		f_sync(&SDFile);
-		f_truncate(&SDFile);
-		f_close(&SDFile);
-		/* 目录层面操作测试 文件夹相关 */
-		f_getcwd((TCHAR*)ReadBufferDir,20);
-		printf("Current Dir %s\r\n",ReadBufferDir);
-		HAL_Delay(50);
-		printf("Make a dir at /\r\n");
-		HAL_Delay(50);
-		f_mkdir("DIR");
-		f_chdir("DIR");
-		printf("OpCodeOpen  %d \r\n",f_open(&SDFile,"SD1.txt",FA_READ | FA_WRITE | FA_OPEN_ALWAYS));
-		HAL_Delay(50);
-		ReadOutCount = 7;
-		f_gets((TCHAR*)ReadBufferSS,ReadOutCount,&SDFile);
-		printf("ReadOut     %s \r\n",ReadBufferSS);
-		HAL_Delay(50);
-		printf("OpCodeWriteS    \r\n");
-		HAL_Delay(50);
-		f_lseek(&SDFile,0);
-		f_printf(&SDFile,"SD.txt");
-		f_sync(&SDFile);
-		f_truncate(&SDFile);
-		f_close(&SDFile);
-		f_getcwd((TCHAR*)ReadBufferDir,20);
-		printf("Current Dir %s\r\n",ReadBufferDir);
+		HAL_Delay(50); //等待系统稳定 不一定需要
 		
-		
-		/* FLASH访问测试 */
-		uint8_t Data = 0;
-		
-		for(uint32_t counter = 0x8000000;counter < (0x8000000 + 10);counter ++)
+		//如果不能mountSD卡 则报错
+		if(f_mount(&SDFatFS,(const TCHAR*)SDPath,1) != FR_OK)
 		{
-			Data = *((uint8_t*)(counter));
-			printf("%02X ",(unsigned int)Data);
-			HAL_Delay(50);
+			printf("SdCard Mount Error : %d\r\n",f_mount(&SDFatFS,(const TCHAR*)SDPath,1));
 		}
-		printf("\r\n");
-		/* FLASH写读测试 */
-		Data = *((uint8_t*)(0x800C000));
-		printf("FlashRead %d\r\n",Data);
 		
-		uint32_t tick = HAL_GetTick();
-		HAL_FLASH_Unlock();
-		//先擦除才能写
-		FLASH_Erase_Sector(3,FLASH_VOLTAGE_RANGE_3);
-		//先清除错误标志位再进行flash写操作 否则报错
-		__HAL_FLASH_CLEAR_FLAG(FLASH_FLAG_PGPERR | FLASH_FLAG_PGSERR); 
-		HAL_FLASH_Program(FLASH_TYPEPROGRAM_BYTE,0x800C000,0xA5);
-		HAL_FLASH_GetError();
-		tick = HAL_GetTick() - tick;
-		printf("time %d\r\n",tick);
-		HAL_FLASH_Lock();
+		//到根目录查询是否有一个叫dload的文件夹 不存在就创建dload文件夹
+		f_chdir("/");
+		if(f_stat("/dload",NULL) != FR_OK)
+		{
+			printf("/dload Not Found,Thus /dload Will Be Create.\r\n");
+			f_mkdir("/dload");
+		}
 		
-		Data = *((uint8_t*)(0x800C000));
-		printf("FlashRead %d\r\n",Data);
-		/* 测试结束 */
+		//检查是否有一个叫 update.bin 的文件在/dload中 如果有就进一步检查是否有 version.txt(ANSI编码) 然后读取第一个字符串进行版本判断
+		//如果没有版本信息 除非触发强制刷写(外部强制更新按钮) 否则不刷写
+		f_chdir("/dload");
+		if(f_stat("update.bin",NULL) != FR_OK)
+		{
+			printf("No update.bin Found,Jump to User Application\r\n");
+			IAP_Jump2App(0x08010000,0x00);
+		}
+		else if(f_stat("update.bin",NULL) == FR_OK)
+		{
+			if(f_stat("version.txt",NULL) != FR_OK)
+			{
+				SdVersion = 0;
+			}
+			else if(f_stat("version.txt",NULL) == FR_OK)
+			{
+				f_open(&SDFile,"version.txt",FA_READ);
+				SdVersion = atoi(f_gets((TCHAR*)String,20,&SDFile));
+				f_close(&SDFile);
+				printf("SdCard Version %d\r\n",SdVersion);
+				HAL_Delay(50);
+				printf("Flash  Version %d\r\n",*((uint16_t*)(0x800C000)));
+				
+				//版本判断 此if语句前一个参数为flash版本 稍后会替换为结构体
+				if(*((uint16_t*)(0x800C000)) > SdVersion)
+				{
+					printf("Flash version is newer than SdCard vertion\r\nUpdate Cancel\r\n");
+					HAL_Delay(50);
+					printf("Jump to User APP.\r\n");
+				}
+				else
+				{
+					printf("Updating...\r\n");
+					HAL_Delay(50);
+					f_open(&SDFile,"update.bin",FA_READ);
+					//获取文件大小 根据文件大小擦除flash 如果文件过大则发出警告 在IAP_FlashErase()中实现
+				  printf("IAP_Erase ERR Code %d\r\n",IAP_FlashErase_APP(f_size(&SDFile)));
+					printf("IAP_Program ERR Code %d\r\n",IAP_FlashProgram(&SDFile));
+					f_close(&SDFile);
+					IAP_Jump2App(0x08010000,0x00);
+				}
+				
+			}				
+		}
+
+
+//    //Debug Code 确认 0x800C000 存的是2010
+//		HAL_Delay(50);
+//		printf("Flash Read %d \r\n",*((uint16_t*)(0x800C000)));
+//		HAL_Delay(50);
+
+
+//		//Debug Code
+//		f_mount(&SDFatFS,(const TCHAR*)SDPath,1);
+//		f_chdir("dload");
+//		//创建update.bin 写入Fcuk You
+//		f_open(&SDFile,"update.bin",FA_CREATE_ALWAYS | FA_READ | FA_WRITE);
+//		f_lseek(&SDFile,0);
+//		f_printf(&SDFile,"Fuck You\r\n");
+//		f_truncate(&SDFile);
+//		f_sync(&SDFile);
+//		f_lseek(&SDFile,0);
+//		f_gets((TCHAR*)String,20,&SDFile);
+//		printf("%s",String);
+//		f_close(&SDFile);
+//		HAL_Delay(50);
+//		//给version.txt写入版本信息进行测试
+//		f_open(&SDFile,"version.txt",FA_CREATE_ALWAYS | FA_READ | FA_WRITE);
+//		f_lseek(&SDFile,0);
+//		f_printf(&SDFile,"2011\r\n");
+//		f_truncate(&SDFile);
+//		f_sync(&SDFile);
+//		f_lseek(&SDFile,0);
+//		f_gets((TCHAR*)String,20,&SDFile);
+//		printf("%s",String);
+//		f_close(&SDFile);
+//		HAL_Delay(50);
+//		//给Flash 0x800C000(sector3 16KB) 地址写入信息方便版本控制比较
+//		HAL_FLASH_Unlock();
+//		//先擦除才能写
+//		FLASH_Erase_Sector(3,FLASH_VOLTAGE_RANGE_3);
+//		//先清除错误标志位再进行flash写操作 否则报错
+//		__HAL_FLASH_CLEAR_FLAG(FLASH_FLAG_PGPERR | FLASH_FLAG_PGSERR); 
+//		HAL_FLASH_Program(FLASH_TYPEPROGRAM_HALFWORD,0x800C000,2010);
+//		printf("Flash Read %d\r\n",*((uint16_t*)(0x800C000)));
+//		HAL_FLASH_Lock();
+		
+		
+		
+//		/* FLASH访问测试 */
+//		uint8_t Data = 0;
+//		
+//		for(uint32_t counter = 0x8000000;counter < (0x8000000 + 10);counter ++)
+//		{
+//			Data = *((uint8_t*)(counter));
+//			printf("%02X ",(unsigned int)Data);
+//			HAL_Delay(50);
+//		}
+//		printf("\r\n");
+//		/* FLASH写读测试 */
+//		Data = *((uint8_t*)(0x800C000));
+//		printf("FlashRead %d\r\n",Data);
+//		
+//		uint32_t tick = HAL_GetTick();
+//		HAL_FLASH_Unlock();
+//		//先擦除才能写
+//		FLASH_Erase_Sector(3,FLASH_VOLTAGE_RANGE_3);
+//		//先清除错误标志位再进行flash写操作 否则报错
+//		__HAL_FLASH_CLEAR_FLAG(FLASH_FLAG_PGPERR | FLASH_FLAG_PGSERR); 
+//		HAL_FLASH_Program(FLASH_TYPEPROGRAM_BYTE,0x800C000,0xA5);
+//		HAL_FLASH_GetError();
+//		tick = HAL_GetTick() - tick;
+//		printf("time %d\r\n",tick);
+//		HAL_FLASH_Lock();
+//		
+//		Data = *((uint8_t*)(0x800C000));
+//		printf("FlashRead %d\r\n",Data);
+//		/* 测试结束 */
 		
 		
 		
@@ -131,7 +176,133 @@ void User_main(void)
 
 
 
+uint8_t IAP_FlashErase_APP(uint32_t ImageSize)
+{
+	//stm32f407vetx app区内存大小 iap程序+配置信息 预计会占用前4个16KB扇区
+	//分别是sec0~sec3 
+	//app从sec4 0x08010000 开始 到 0x0807FFFF 结束
+	//想适配其他芯片 本flash擦除函数需要根据其他芯片的情况具体修改
+	
+	FLASH_EraseInitTypeDef flasherase;
+	uint32_t Flash_Erase_ErrorSector = 0;
+	
+	flasherase.Banks = FLASH_BANK_1;
+	flasherase.NbSectors = 0;
+	flasherase.Sector = FLASH_SECTOR_4;
+	flasherase.TypeErase = FLASH_TYPEERASE_SECTORS;
+	flasherase.VoltageRange = FLASH_VOLTAGE_RANGE_3;
+	
+	printf("Sector start from 4\r\n");
+	
+	//image过大
+	if(ImageSize > 458752)
+	{
+		printf("ImageSize too Large.\r\n");
+		return 1;
+	}
+	else if(ImageSize > 327680)
+	{
+		printf("All sector erase.\r\n");
+		printf("Sector Erase to Sector 6\r\n");
+		//扇区擦除部分
+		HAL_FLASH_Unlock();		
+		flasherase.NbSectors = 4;
+		HAL_FLASHEx_Erase(&flasherase,&Flash_Erase_ErrorSector);
+		HAL_FLASH_Lock();
+		return 0;
+	}
+	else if(ImageSize > 196608)
+	{
+		printf("Sector Erase to Sector 6\r\n");
+		//扇区擦除部分
+		HAL_FLASH_Unlock();
+		flasherase.NbSectors = 3;
+		HAL_FLASHEx_Erase(&flasherase,&Flash_Erase_ErrorSector);
+		HAL_FLASH_Lock();
+		return 0;		
+	}
+	else if(ImageSize > 65536)
+	{
+		printf("Sector Erase to Sector 5\r\n");
+		//扇区擦除部分
+		HAL_FLASH_Unlock();
+		flasherase.NbSectors = 2;
+		HAL_FLASHEx_Erase(&flasherase,&Flash_Erase_ErrorSector);
+		HAL_FLASH_Lock();
+		return 0;				
+	}
+	else if(ImageSize <= 65536)
+	{
+		printf("Sector Erase to Sector 4\r\n");
+		//扇区擦除部分
+		HAL_FLASH_Unlock();
+		flasherase.NbSectors = 1;
+		HAL_FLASHEx_Erase(&flasherase,&Flash_Erase_ErrorSector);
+		HAL_FLASH_Lock();
+		return 0;
+	}
+	
+	return 0;
+}
 
+
+uint8_t IAP_FlashProgram(FIL* file)
+{
+	uint32_t ImageSize = 0;
+	uint32_t ReadCount = 0;
+	uint8_t  ReadBuffer = 0;
+	
+	//已经提前f_open()过了
+	printf("Flash program Start.\r\n");
+	HAL_Delay(50);
+	ImageSize = f_size(&SDFile);
+	printf("ImageSize %d.\r\n",ImageSize);
+	HAL_Delay(50);
+	printf("Flash Program Start AT 0x0801 0000.\r\n");
+	HAL_Delay(50);
+	
+	//移动文件指针到文件开头
+	f_lseek(&SDFile,0);
+	
+	HAL_FLASH_Unlock();
+	
+	for(uint32_t counter = 0;counter < ImageSize;counter ++)
+	{
+		//读文件一个字节 文件指针自动增加
+		f_read(&SDFile,&ReadBuffer,1,&ReadCount);
+		__HAL_FLASH_CLEAR_FLAG(FLASH_FLAG_PGPERR | FLASH_FLAG_PGSERR);
+		//字节模式编程flash基地址+counter 编程内容为读出来的内容
+		HAL_FLASH_Program(FLASH_TYPEPROGRAM_BYTE,0x08010000 + counter,ReadBuffer);	
+	}
+	
+	HAL_FLASH_Lock();
+	
+	
+	return 0;
+}
+
+
+void IAP_Jump2App(uint32_t AppFlashBase,uint32_t MSP_Addr)
+{
+	uint32_t JumpAddr = 0;
+	void (*pFun)(void); //定义一个函数指针.用于指向APP程序入口
+	
+	//目前设置MSP暂时没有用 默认内部SRAM默认地址为堆栈
+	MSP_Addr = 0;
+	
+	//APP程序要跳转到复位向量 也就是App基地址+4的地址
+	JumpAddr = *(uint32_t *)( AppFlashBase + 4);
+	
+	//生成跳转函数.将复位中断向量地址做为函数指针
+	pFun = (void (*)(void))JumpAddr;
+	
+	//关闭所有外设
+	HAL_DeInit();
+		
+	//执行用户App
+	(*pFun)();
+	
+}
 
 #ifdef __GNUC__
 #define PUTCHAR_PROTOTYPE int __io_putchar(int ch)
